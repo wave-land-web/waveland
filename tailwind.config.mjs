df54db7a1c302ba97ab99a2b05726bc358dc4cdf
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-05-02 15:09:33 -0500

    basic global, nav and hero styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,22 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    colors: {
+      white: '#d5deeb',
+      darkBlue: '#011627',
+      purple: '#c792e9',
+      green: '#7fdbca',
+      orange: '#ff757f',
+    },
     fontFamily: {
       body: ['Poppins Regular', 'sans-serif'],
       mono: ['Monaspace Argon Var', 'monospace'],
+    },
+    extend: {
+      strokeWidth: {
+        1: '1rem',
+        2: '2rem',
+      },
     },
   },
   plugins: [],
