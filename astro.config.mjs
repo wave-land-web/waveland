63ba97b8d7101d31211d378a0ae58e2b905a0b8b
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-05-11 13:11:49 -0500

    fix flash of unstyled content (swithc to output: 'server')

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wavelandweb.com',
-  output: 'hybrid',
+  output: 'server',
   scopedStyleStrategy: 'class',
   integrations: [
     sitemap({
