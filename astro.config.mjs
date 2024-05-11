ae6c431775c99f366274b9054e22454c2d9dab34
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-05-11 13:21:07 -0500

    switch to hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wavelandweb.com',
-  output: 'server',
+  output: 'hybrid',
   scopedStyleStrategy: 'class',
   integrations: [
     sitemap({
