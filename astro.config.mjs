a9b21f2a9ffbd6121c17fb886246b6b933a90b6c
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-05-15 17:40:01 -0500

    add robots.txt and sitemap link in head of document

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { remarkReadingTime } from './src/util/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
+  // TODO: Update this to your site's URL (https://wavelandweb.com/) before deploying
   site: 'https://waveland-redesign.netlify.app/',
   output: 'server',
   prefetch: true,
