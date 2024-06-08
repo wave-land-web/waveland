adbdcb3e010b82a61fc4b1dfa23923acaafa7bb6
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-06-08 09:52:31 -0500

    fix(app): Fix container widths and padding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,8 @@ export default {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1440px',
+      '3xl': '1600px',
+      '4xl': '1920px',
       'custom-height-mq': {
         raw: '(max-height: 680px)',
       },
