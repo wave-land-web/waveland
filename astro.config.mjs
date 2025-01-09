c6eda3a3095e4229ba24febd8b1babe139746511
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-01-09 14:47:03 -0600

    chore(app): upgrade Astro deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wavelandweb.com/',
-  output: 'hybrid',
   prefetch: {
     prefetchAll: true,
   },
