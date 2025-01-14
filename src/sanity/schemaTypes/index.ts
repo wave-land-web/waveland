8215b7e340f5e96b1bc9c9a129d3ce0e6efc8e18
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-01-14 10:45:07 -0600

    feat(app): case study header logic + styling, minimal css refactor

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,5 @@
 import type { SchemaTypeDefinition } from 'sanity'
-import blockContent from './blockContent'
+import blockContent from './blockContent.ts'
 import caseStudy from './case-study'
 import post from './post'
 import tag from './tag'
