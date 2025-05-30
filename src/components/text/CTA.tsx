eab0698f97daeb8debf8825a1adfea4170d25a78
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-05-30 14:33:57 -0500

    feat(app): about section, global styling, inputs, containers

--- src/components/text/CTA.tsx
+++ src/components/text/CTA.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const baseClasses =
-  'px-4 py-3 border border-purple rounded-md shadow-sm text-purple hover:text-black hover:bg-purple transition-all duration-(--transition) ease-in-out text-center'
+  'px-4 py-3 border border-purple border-dashed rounded-lg shadow-lg text-purple hover:text-black hover:bg-purple transition-all duration-(--transition) ease-in-out text-center'
 
 export default function CTA({ tag, href, text, className, isActive, onClick, ...rest }: Props) {
   const activeClasses = isActive ? '!text-black bg-purple' : ''
